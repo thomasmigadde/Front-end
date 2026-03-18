2b13a3956343e4bb65b2a02796564c255e25d02c
THOMAS MIGADDE <thomasmigadde@gmail.com>
2026-03-18 18:05:31 +0300

    Add files via upload
    My react works

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Message from './Message.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Message />);
